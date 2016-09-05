3fbd95aa6a941527a65a3937a04ad2655b3ffbba
Stefan Untereichner <stefan.untereichner@gmail.com>
2016-09-05 14:01:13 +0200

    include junit-test execution of keyword-driven tests within ant-file

---
+++ Silk4J/src/KDTTestSuite.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+
+import com.borland.silktest.jtf.keyworddriven.KeywordTestSuite;
+import com.borland.silktest.jtf.keyworddriven.KeywordTests;
+
+@RunWith(KeywordTestSuite.class)
+@KeywordTests({ "Agent Lookup", "LoginTest" })
+public class KDTTestSuite {
+
+}
